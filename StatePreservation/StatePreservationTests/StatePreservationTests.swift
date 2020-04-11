c0d64dc4352a3ca6d5238cbd23d917a156143016
Tigran2662 <tigran.simonyan@picsart.com>
2020-04-11 19:07:04 +0400

    Added empty project

---
+++ StatePreservation/StatePreservationTests/StatePreservationTests.swift
@@ -0,0 +1,34 @@
+//
+//  StatePreservationTests.swift
+//  StatePreservationTests
+//
+//  Created by Tigran Simonyan on 4/11/20.
+//  Copyright © 2020 Scriptomania. All rights reserved.
+//
+
+import XCTest
+@testable import StatePreservation
+
+class StatePreservationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
